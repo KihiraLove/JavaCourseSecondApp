1e78e3da876fa0ae856574fb8332e8cc03b86b80
KihiraLove <illuminatecmdc@gmail.com>
2020-12-18 18:06:38 +0100

    DataBank added

--- WebShopApp/src/main/java/com/pb8jv3/java1/webshopapp/filemanager/utility/MonitorsGenerator.java
+++ WebShopApp/src/main/java/com/pb8jv3/java1/webshopapp/filemanager/utility/MonitorsGenerator.java
@@ -29,28 +29,17 @@ public class MonitorsGenerator {
     private static Monitor generateMonitor(){
 	Random random = new Random();
 	
-	String[] manufacturers = {"ASUS", "LG", "BenQ", "AOC Monitors", "Dell",
-				    "Samsung Engineering", "Acer Inc.",
-				    "Sony Corporation", "ViewSonic", "HP",
-				    "Eizo", "Philips", "Iiyama", "Apple"};
-	
-	String[] aspectRatios = {"1024x768", "1600x1200", "1280x1024",
-				    "2160x1440", "1920x1080", "2560x1080",
-				    "5120x1440"};
-	
-	Integer[] refreshRates = {60, 75, 144, 165, 240, 100, 120};
-	
-	return new Monitor(manufacturers[random.nextInt(manufacturers.length)],
+	return new Monitor(DataBank.MANUFACTURERS[random.nextInt(DataBank.MANUFACTURERS.length)],
 		    random.ints(48, 123)
 			    .filter(ri -> (ri <= 57 || ri >= 65) && (ri <= 90 || ri >= 97))
 			    .limit(8)
 			    .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
 			    .toString(),
-		    aspectRatios[random.nextInt(aspectRatios.length)],
+		    DataBank.ASPECT_RATIOS[random.nextInt(DataBank.ASPECT_RATIOS.length)],
 		    new DisplaySize(random.nextInt(40) + 60, random.nextInt(30) + 20),
 		    random.nextInt(98000) + 2000,
 		    DisplayType.getRandomType(),
-		    refreshRates[random.nextInt(refreshRates.length)],
+		    DataBank.REFRESH_RATES[random.nextInt(DataBank.REFRESH_RATES.length)],
 		    random.nextInt(600));
     }
 }
