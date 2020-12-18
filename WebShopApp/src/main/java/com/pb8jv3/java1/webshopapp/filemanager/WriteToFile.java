b9fe95a93f089a7b7b55dc75494d79e6d2f65537
KihiraLove <illuminatecmdc@gmail.com>
2020-12-18 18:13:11 +0100

    Update WriteToFile.java

--- WebShopApp/src/main/java/com/pb8jv3/java1/webshopapp/filemanager/WriteToFile.java
+++ WebShopApp/src/main/java/com/pb8jv3/java1/webshopapp/filemanager/WriteToFile.java
@@ -4,8 +4,10 @@ import com.pb8jv3.java1.webshopapp.filemanager.utility.FileLocation;
 import com.pb8jv3.java1.webshopapp.datamanager.WishlistManager;
 import com.pb8jv3.java1.webshopapp.datamanager.ProductManager;
 import com.pb8jv3.java1.webshopapp.datamanager.CartManager;
+import com.pb8jv3.java1.webshopapp.datamanager.data.Monitor;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Map;
 
 /**
  *
@@ -17,42 +19,20 @@ public class WriteToFile {
     }
     
     public static void write(ProductManager productManager) {
-    	try (FileWriter writer = new FileWriter(FileLocation.PRODUCT_DATA_FILE_LOCATION)) {
-	    productManager.getProducts().entrySet().forEach(entry -> {
-		try {
-		    writer.write(entry.getKey() + ";" + entry.getValue().printable());
-		    writer.write(System.lineSeparator());
-		} catch (IOException e) {
-		    System.out.println("An error occurred: " + e.getMessage());
-		}
-		});
-	    System.out.println("Product data written to " + FileLocation.PRODUCT_DATA_FILE_NAME);
-	    writer.close();
-	} catch (IOException e) {
-	    System.out.println("An error occurred: " + e.getMessage());
-	}
+	WriteToFile.writer(productManager.getProducts(), FileLocation.PRODUCT_DATA_FILE_LOCATION);
     }
     
     public static void write(CartManager cartManager) {
-    	try (FileWriter writer = new FileWriter(FileLocation.CART_DATA_FILE_LOCATION)) {
-	    cartManager.getProducts().entrySet().forEach(entry -> {
-		try {
-		    writer.write(entry.getKey() + ";" + entry.getValue().printable());
-		    writer.write(System.lineSeparator());
-		} catch (IOException e) {
-		    System.out.println("An error occurred: " + e.getMessage());
-		}
-		});
-	    System.out.println("Product data written to " + FileLocation.CART_DATA_FILE_NAME);
-	    writer.close();
-	} catch (IOException e) {
-	    System.out.println("An error occurred: " + e.getMessage());
-	}
+	WriteToFile.writer(cartManager.getProducts(), FileLocation.CART_DATA_FILE_LOCATION);
     }
     
     public static void write(WishlistManager wishlistManager) {
-    	try (FileWriter writer = new FileWriter(FileLocation.WISHLIST_DATA_FILE_LOCATION)) {
-	    wishlistManager.getProducts().entrySet().forEach(entry -> {
+	WriteToFile.writer(wishlistManager.getProducts(), FileLocation.WISHLIST_DATA_FILE_LOCATION);
+    }
+    
+    private static void writer(Map<Integer, Monitor> monitors, String fileLocation){
+        try (FileWriter writer = new FileWriter(fileLocation)) {
+	    monitors.entrySet().forEach(entry -> {
 		try {
 		    writer.write(entry.getKey() + ";" + entry.getValue().printable());
 		    writer.write(System.lineSeparator());
@@ -60,7 +40,7 @@ public class WriteToFile {
 		    System.out.println("An error occurred: " + e.getMessage());
 		}
 		});
-	    System.out.println("Product data written to " + FileLocation.WISHLIST_DATA_FILE_NAME);
+	    System.out.println("Product data written to " + fileLocation);
 	    writer.close();
 	} catch (IOException e) {
 	    System.out.println("An error occurred: " + e.getMessage());
